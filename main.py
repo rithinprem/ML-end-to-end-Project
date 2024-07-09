220f7ee1ecc36d64be2a037a03dfe5fefd4d3a1f
rithin_prem <rithinengineeringmy@gmail.com>
2024-07-09 19:48:51 +0530

    Data transformation done

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+
 
 
 
@@ -23,6 +25,19 @@ try:
         logger.info(f">>>>>>>>>>>{STAGE_NAME} started <<<<<<<<<<<<<")
         data_validation = DataValidationTrainingPipeline()
         data_validation.main()
+        logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<<<<")
+
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+
+try:
+        logger.info(f">>>>>>>>>>>{STAGE_NAME} started <<<<<<<<<<<<<")
+        data_transformation = DataTransformationTrainingPipeline()
+        data_transformation.main()
         logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<<<<")
 
 except Exception as e:
