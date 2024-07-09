658275ae023a8572515e1c59ca6a91f929cc0a59
rithin_prem <rithinengineeringmy@gmail.com>
2024-07-09 17:41:47 +0530

    Data validation done

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from mlProject import logger
-from mlProject.pipeline.state_01_data_ingestion import DataIngestionTrainingPipeline
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -8,6 +10,19 @@ try:
         logger.info(f">>>>>>>>>>>{STAGE_NAME} started <<<<<<<<<<<<<")
         data_ingestion = DataIngestionTrainingPipeline()
         data_ingestion.main()
+        logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<<<<")
+
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Validation stage"
+
+try:
+        logger.info(f">>>>>>>>>>>{STAGE_NAME} started <<<<<<<<<<<<<")
+        data_validation = DataValidationTrainingPipeline()
+        data_validation.main()
         logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<<<<")
 
 except Exception as e:
